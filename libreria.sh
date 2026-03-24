f7abe12d76eb2593d0f2f80303f71246dda0d190
Jesús Figueroa Roldán <verdeblancoyazul@gmail.com>
2026-03-24 20:31:35 +0100

    Update library header comment for clarity

--- libreria.sh
+++ libreria.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ============================================================
-# Librería de funciones de administración de sistemas
+# Librería de funciones
 # Autores:
 #   - Jesús Figueroa Roldán
 #   - Iván Jiménez Moreno
